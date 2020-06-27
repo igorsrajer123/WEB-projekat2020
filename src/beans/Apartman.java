b3704fb378ec1d8a2b49603f049a1674e8f85afa
Srdjan Savic <ssavic997@gmail.com>
2020-06-27 17:32:16 +0200

    izmene u Apartman(beans)

--- src/beans/Apartman.java
+++ src/beans/Apartman.java
@@ -1,56 +1,62 @@
 package beans;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 public class Apartman {
 
-	public enum Tip {Soba, Ceo_apartman}
-	public enum Status {Aktivno, Neaktivno}
-	
+	public enum Tip {
+		Soba, Ceo_apartman
+	}
+
+	public enum Status {
+		Aktivno, Neaktivno
+	}
+
 	private Tip tip;
 	private int brSoba;
 	private int brGostiju;
 	private Lokacija lokacija;
-	private String datumZaIzdavanje;
-	private List<String> dostupnostPoDatumima;
+	private ArrayList<Date> datumiZaIzdavanje;
+	private ArrayList<Date> dostupnostPoDatumima;
 	private String domacin;
 	private Komentar komentar;
-	private String slika;//vise slika treba
+	private String slika;// vise slika treba
 	private double cenaPoNoci;
 	private String vrZaPrijavu;
 	private String vrZaOdjavu;
 	private Status status;
 	private ArrayList<SadrzajApartmana> sadrzajAp;
 	private List<Rezervacija> rezervacije;
-	
+
 	private Boolean uklonjen = false;
 	private String idApartmana = UUID.randomUUID().toString();
-	
+
 	public Apartman() {
 		super();
 		this.status = Status.Aktivno;
 	}
-	
-	public Apartman(Tip tip, int brSoba, int brGostiju, Lokacija lokacija, String datum, String d,
-						String slika, ArrayList<SadrzajApartmana> sadrzaj, double cena, Boolean uklonjen, String id) {							
+
+	public Apartman(Tip tip, int brSoba, int brGostiju, Lokacija lokacija, ArrayList<Date> datum, String d,
+			String slika, ArrayList<SadrzajApartmana> sadrzaj, double cena, Boolean uklonjen, String id) {
 		super();
 		this.tip = tip;
 		this.brSoba = brSoba;
 		this.brGostiju = brGostiju;
 		this.lokacija = lokacija;
-		this.datumZaIzdavanje = datum;
-		this.dostupnostPoDatumima = new ArrayList<String>();
+		this.datumiZaIzdavanje = datum;
+		this.dostupnostPoDatumima = new ArrayList<Date>();
 		this.domacin = d;
 		this.komentar = null;
 		this.slika = slika;
 		this.cenaPoNoci = cena;
 		this.vrZaOdjavu = "";
 		this.vrZaPrijavu = "";
-		this.status = Status.Aktivno;	
+		this.status = Status.Aktivno;
 		this.sadrzajAp = new ArrayList<SadrzajApartmana>();
-		this.rezervacije = new ArrayList<Rezervacija>();	
+		this.rezervacije = new ArrayList<Rezervacija>();
 		this.uklonjen = uklonjen;
 		this.idApartmana = id;
 	}
@@ -70,10 +76,11 @@ public class Apartman {
 	@Override
 	public String toString() {
 		return "Apartman [tip=" + tip + ", brSoba=" + brSoba + ", brGostiju=" + brGostiju + ", lokacija=" + lokacija
-				+ ", datumZaIzdavanje=" + datumZaIzdavanje + ", dostupnostPoDatumima=" + dostupnostPoDatumima
+				+ ", datumiZaIzdavanje=" + datumiZaIzdavanje + ", dostupnostPoDatumima=" + dostupnostPoDatumima
 				+ ", domacin=" + domacin + ", komentar=" + komentar + ", slika=" + slika + ", cenaPoNoci=" + cenaPoNoci
 				+ ", vrZaPrijavu=" + vrZaPrijavu + ", vrZaOdjavu=" + vrZaOdjavu + ", status=" + status + ", sadrzajAp="
-				+ sadrzajAp + ", rezervacije=" + rezervacije + ", uklonjen=" + uklonjen + ", idApartmana=" + idApartmana + "]";
+				+ sadrzajAp + ", rezervacije=" + rezervacije + ", uklonjen=" + uklonjen + ", idApartmana=" + idApartmana
+				+ "]";
 	}
 
 	public void setBrSoba(int brSoba) {
@@ -96,19 +103,19 @@ public class Apartman {
 		this.lokacija = lokacija;
 	}
 
-	public String getDatumZaIzdavanje() {
-		return datumZaIzdavanje;
+	public ArrayList<Date> getdatumiZaIzdavanje() {
+		return datumiZaIzdavanje;
 	}
 
-	public void setDatumZaIzdavanje(String datumZaIzdavanje) {
-		this.datumZaIzdavanje = datumZaIzdavanje;
+	public void setdatumiZaIzdavanje(ArrayList<Date> datumiZaIzdavanje) {
+		this.datumiZaIzdavanje = datumiZaIzdavanje;
 	}
 
-	public List<String> getDostupnostPoDatumima() {
+	public ArrayList<Date> getDostupnostPoDatumima() {
 		return dostupnostPoDatumima;
 	}
 
-	public void setDostupnostPoDatumima(List<String> dostupnostPoDatumima) {
+	public void setDostupnostPoDatumima(ArrayList<Date> dostupnostPoDatumima) {
 		this.dostupnostPoDatumima = dostupnostPoDatumima;
 	}
 
