4f239c8aa3806e34900ce920cf5f04335db3e4a8
igorsrajer123 <57237025+igorsrajer123@users.noreply.github.com>
2020-07-01 14:18:13 +0200

    Kreiranje i izlistavanje svih komentara

--- src/services/KomentarServis.java
+++ src/services/KomentarServis.java
@@ -1,10 +1,13 @@
 package services;
 
+import java.util.ArrayList;
+
 import javax.annotation.PostConstruct;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -14,8 +17,10 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import beans.Apartman;
+import beans.Domacin;
 import beans.Gost;
 import beans.Komentar;
+import beans.Korisnik;
 import dao.ApartmanDAO;
 import dao.KomentarDAO;
 import dao.KorisnikDAO;
@@ -50,14 +55,42 @@ public class KomentarServis {
 		//dodaj komentar u kontekst komentara
 		dao.dodajKomentar(k);
 		
+		//dodaj komentar apartmanu kom je namenjen u listu apartmana
+		Apartman a = apartmaniDAO.getPoIdApartmana(idAp);
+		a.dodajKomentar(k);
 		
+		//dodaj komentar u apartman kod domacina
+		String korImeDomacina = a.getDomacin();
+		Korisnik kor = korisniciDAO.getOneKorisnik(korImeDomacina);
+		System.out.println("Korisnicko ime domacina apartmana na kom je komentar: " + korImeDomacina);
 		
+		Domacin d = (Domacin) kor;
 		
-		
+		(d.getApartmanPoId(idAp)).dodajKomentar(k);
+			
 		dao.sacuvajKomentare();
+		apartmaniDAO.sacuvajApartmane();
+		korisniciDAO.sacuvajKorisnika();
 		
 		System.out.println("Komentar uspesno kreiran!");
 		return Response.ok().build();
+	}
+	
+	@GET
+	@Path("/getSveKomentare")
+	@Produces(MediaType.APPLICATION_JSON)
+	public ArrayList<Komentar> getSveKomentare(){
+		
+		KomentarDAO dao = (KomentarDAO) ctx.getAttribute("komentarDAO");
+		
+		if(dao == null){
+			System.out.println("Komentari prazni!");
+			return null;
+		}
+		
+		ArrayList<Komentar> lista = dao.getSveKomentare();
+		
+		return lista;
 	}
 	
 	
